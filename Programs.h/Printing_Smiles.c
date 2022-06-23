90cadc84b11db12b365630bbc80b253dce023685
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-06-23 22:53:39 +0530

    Add files via upload

---
+++ Programs.h/Printing_Smiles.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int r,c;
+    for(r=1;r<=21;r++)
+    { 
+        for(c=1;c<=80;c++)
+     { 
+         printf("%c", 1);
+     }
+        printf("\n");
+    }
+    return 0;
+}
