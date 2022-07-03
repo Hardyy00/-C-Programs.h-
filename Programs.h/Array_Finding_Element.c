d6bbad9f054a809d2e877a0b191d7ff923121f5f
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-07-03 22:53:25 +0530

    Add files via upload

---
+++ Programs.h/Array_Finding_Element.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int i,n,a[100],p=0,num;
+    
+    printf("\nEnter the size of the array : ");
+    scanf("%d",&n);
+
+    printf("\nEnter the elements :\n");
+    for(i=0;i<n;i++)
+      scanf("%d",&a[i]);
+
+    printf("\nEnter the number you want to find : ");
+    scanf("%d",&num);
+    for(i=0;i<n;i++)
+       if(num==a[i])
+          {
+              p=i;
+              break;
+          }
+    
+    if(p==0)
+        printf("\nNot found");
+    else
+       printf("\nPosition of element %d (with respect to array) is %d",num,p);
+
+       return 0;
+}
