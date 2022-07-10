fce19d442b23aef14c8265f01f0206cad2ba7a9e
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-07-10 23:10:25 +0530

    Add files via upload

---
+++ Programs.h/String_Replacing_Character_First_Occurence_With_Another.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main()
+{
+    char a[100],ch;
+    int i,flag=0,p;
+
+    printf("\nEnter a string : ");
+       gets(a);
+
+    printf("\nEnter the character that you want to replace : ");
+       scanf("%c",&ch);
+    
+    for(i=0;a[i]!=0;i++)
+      if(a[i]==ch)
+          {
+              flag=1;
+              p=i;
+              break;
+          }
+
+    if(flag==0)
+          printf("\nThe character doesn't exist in the string.");
+    else 
+       {
+           fflush(stdin);
+
+           printf("\nEnter the character that you want the character '%c' to be replaced with : ",ch);
+             scanf("%c",&ch);
+
+            a[p]=ch;
+
+            printf("\nString after replacing : %s",a);         
+       }
+
+       return 0;
+}
