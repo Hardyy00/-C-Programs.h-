c0632a493d3bf1ae4d828055eebd8df7bf7b9372
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-07-10 23:11:32 +0530

    Add files via upload

---
+++ Programs.h/String_Word_Last_Occurence.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+    char a[100],b[100];
+    int i,j,flag=0,p;
+
+    printf("\nEnter a string : ");
+      gets(a);
+    
+    printf("\nEnter the word that you want to search : ");
+       gets(b);
+
+    for(i=0;a[i]!='\0';i++)
+      if(a[i]==b[0])
+          {
+              flag=1;
+
+              for(j=1;b[j]!='\0';j++)
+                 if(a[i+j]!=b[j])
+                    flag=0;
+                
+                if(flag==1)
+                      p=i;
+          }
+
+        if(flag==0)
+           printf("\nThe word doesn't exist in the string");
+        else
+           printf("\nThe last occurence of the word '%s' is at position %d",b,p+1);
+             
+
+             return 0;
+}
