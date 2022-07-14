3074ac547a08e789d802c623406c7139ebf0f654
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-07-14 23:18:32 +0530

    Add files via upload

--- Programs.h/Multiplication_Table.c
+++ Programs.h/Multiplication_Table.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 int main()
-
 {
-  
-  int num,i;
-  
-  printf("\nEnter the number you want multiplication of : ");
-  scanf("%d", &num);
-  
-  printf("\nMultiplication of %d is as follows :\n", num);
-  
-  for(i=1;i<=10;i++)
-  {
-     printf("%d * %d = %d\n", num, i, num*i);
-  }
-  
-  return 0;
+   int num,i;
+   printf("\nEnter the number you want multiplication of : ");
+   scanf("%d",&num);
+
+   printf("\nMultiplication of %d is as follows :\n");
+
+   for(i=1;i<=10;i++)
+   {
+      printf("%d * %d = %d\n",num,i,num*i);
+   }
+
+   return 0;
+
 }
