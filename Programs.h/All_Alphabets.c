da57e76c46f92343ea4010e5ae1dbb131a38900c
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-07-14 23:17:42 +0530

    Add files via upload

---
+++ Programs.h/All_Alphabets.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+   int ch=97;
+   printf("\nAlphabets from a to z are :\n");
+
+   while(ch<=122)
+   {
+      printf("%c\n",ch);
+      ch++;
+
+   }
+          return 0;
+}
