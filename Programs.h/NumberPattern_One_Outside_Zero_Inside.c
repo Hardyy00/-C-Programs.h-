49482703f2ee0406ac3babee5cc368e7562cde91
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-07-17 23:27:16 +0530

    Add files via upload

---
+++ Programs.h/NumberPattern_One_Outside_Zero_Inside.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+   int i,j,r,c;
+   printf("\nEnter the number of rows and columns :\n");
+   scanf("%d%d",&r,&c);
+
+   for(i=1;i<=r;i++)
+   {
+      for(j=1;j<=c;j++)
+      {
+          if(i==1 || i==c)
+                    printf("1 ");
+          else 
+             {
+               if(j==1 || j==c)
+                          printf("1 ");
+               else
+                   printf("0 ");
+             }
+
+      }
+      printf("\n");
+   }
+        return 0;
+}
