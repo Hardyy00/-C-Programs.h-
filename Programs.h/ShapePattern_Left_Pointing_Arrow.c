6b22bb0360f9b69966ff09ff9d0b516ee1014041
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-07-22 23:28:50 +0530

    Add files via upload

---
+++ Programs.h/ShapePattern_Left_Pointing_Arrow.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int i,j,space;
+
+    for(i=1;i<=5;i++)
+    {
+        for(space=1;space<=5-i;space++)
+             printf(" ");
+      
+        for(j=1;j<=5-(i-1);j++)
+            printf("*");
+        
+        printf("\n");
+    }
+      
+      for(i=1;i<=5;i++)
+      {
+          for(space=1;space<=i;space++)
+              printf(" ");
+
+          for(j=1;j<=i+1;j++)
+              printf("*");
+
+            printf("\n");
+      }
+          return 0;
+}
