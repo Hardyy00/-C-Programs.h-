e0dedf8a9e31157829edb7b99583ec4eada7c510
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-07-23 23:29:57 +0530

    Add files via upload

---
+++ Programs.h/ShapePattern_Upside_Down_Hollow_Triangle.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    int i,j,space;
+
+    for(i=1;i<=5;i++)
+    {
+        if(i==1)
+           for(j=1;j<=9;j++)
+              printf("*");
+
+        else
+           {
+               for(space=1;space<=i-1;space++)
+                 printf(" ");
+
+                printf("*");
+
+                for(space=1;space<=5-(i-2)*2;space++)
+                     printf(" ");
+
+                    if(i==5);
+
+                    else
+                        printf("*");
+           }
+             printf("\n");
+    }
+       return 0;
+}
