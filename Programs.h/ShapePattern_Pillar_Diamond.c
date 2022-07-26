a70a34fad53f3d40a2ff14d3ea37d2a64d04d3d1
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-07-26 23:14:19 +0530

    Add files via upload

---
+++ Programs.h/ShapePattern_Pillar_Diamond.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    int i,j;
+
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+            if(i==1 || i==5)
+               printf("*");
+
+            else if(i==2 || i==4)
+                {
+                    if(j==3)
+                       printf(" ");
+                    else 
+                        printf("*");
+                }
+            
+            else
+                {
+                    if(j==2 || j==4)
+                       printf(" ");
+                    else 
+                       printf("*");
+                }
+        }
+          printf("\n");
+    }
+       return 0;
+}
