76485ba3f6f51fda3074f946fc162caee4d4e316
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-07-28 23:17:31 +0530

    Add files via upload

---
+++ Programs.h/String_toLowerCase.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    char a[100];
+    int i;
+
+    printf("\nEnter a string : ");
+     gets(a);
+
+     for(i=0;a[i]!='\0';i++)
+        if(a[i]>=65 && a[i]<=90)
+             a[i]=a[i]+32;
+
+    printf("\nThe string is : %s",a);
+
+    return 0;
+}
