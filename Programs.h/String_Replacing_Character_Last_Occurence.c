39655afe750de8f44f5fb127a273c1372b899b8c
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-08-03 23:19:56 +0530

    Add files via upload

---
+++ Programs.h/String_Replacing_Character_Last_Occurence.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main()
+{
+    char a[100],ch;
+    int i,p,flag=0;
+
+    printf("\nEnter a string : ");
+      gets(a);
+
+    printf("\nEnter the character that you want to replace : ");
+      scanf("%c",&ch);
+
+    for(i=0;a[i]!='\0';i++)
+        if(a[i]==ch)
+          {
+              flag=1;
+              p=i;
+          }
+
+    if(flag==0)
+           printf("\nThe character doesn't exist in the string");
+    else
+       {
+           fflush(stdin);
+           
+           printf("\nEnter the character that you want the character '%c' to be replaced with : ",ch);
+            scanf("%c",&ch);
+
+            a[p]=ch;
+
+           printf("\nAfter the replacing ,the string is : %s",a);
+       }
+
+       return 0;
+}
