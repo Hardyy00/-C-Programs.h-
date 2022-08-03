1c090b30dcd9f3ef4270fa22cf89209798820275
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-08-03 23:19:18 +0530

    Add files via upload

--- Programs.h/String_Lowest_Frequency_Character.c
+++ Programs.h/String_Lowest_Frequency_Character.c
@@ -2,32 +2,31 @@
 int main()
 {
     char a[100];
-    int i,b[200]={0},ch,f,max;
+    int i,b[200]={0},max,f,p;
 
     printf("\nEnter a string : ");
-      gets(a);
+     gets(a);
 
-      max=a[0];
+    max=a[0];
+    for(i=0;a[i]!='\0';i++)
+       if(max<a[i])
+           max=a[i];
 
-    for(i=0;a[i]!=0;i++)
-      if(max<a[i])
-         max=a[i];
-        
     max++;
-
+    
     for(i=0;a[i]!='\0';i++)
       b[a[i]]++;
 
     f=b[a[0]];
-   
-     for(i=0;i<max;i++)
-       if(f>b[i] && b[i]>0)
+    p=a[0];
+    for(i=0;i<max;i++)
+      if(f>b[i] && b[i]>0)
            {
                f=b[i];
-               ch=i;
+               p=i;
            }
 
-    printf("\nLowest frequency character is '%c' with the frequency of %d",ch,f);
+    printf("\nLowest Frequency character is '%c' with the frequency of %d",p,f);
 
     return 0;
 }
