1e554dce54caa9ca126677bfb2d8b763fcc9582f
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-08-03 23:18:59 +0530

    Add files via upload

--- Programs.h/String_Highest_Frequency_Character.c
+++ Programs.h/String_Highest_Frequency_Character.c
@@ -2,32 +2,35 @@
 int main()
 {
     char a[100];
-
-    int i,max,b[200]={0},f,ch;
+    int i,max,b[500]={0},f,p;
 
     printf("\nEnter a string : ");
-      gets(a);
-   
+     gets(a);
+
     max=a[0];
 
-   for(i=0;a[i]!=0;i++)
-       if(max<a[i])
-          max=a[i];
+    for(i=0;a[i]!='\0';i++)
+      if(max<a[i])
+         max=a[i];
 
-        max++;
+    max++;
 
     for(i=0;a[i]!=0;i++)
-      b[a[i]]++;
-
+        b[a[i]]++;
+    
+    
     f=b[0];
+    p=0;
+    
     for(i=0;i<max;i++)
-        if(f<b[i])
-           {
-               f=b[i];
-               ch=i;
-           }
+       if(f<b[i])
+       {
+          f=b[i];
+          p=i;
+       }
 
-    printf("\nHighest frequency character is '%c' with frequency of %d",ch,f);
+    printf("\nThe highest frequency charater is '%c' with the frequency of %d",p,f);
 
-     return 0;
+    return 0;
+         
 }
