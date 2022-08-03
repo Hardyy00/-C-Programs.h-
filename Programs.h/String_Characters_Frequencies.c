d4de060acc2d4ed1220e36390bbbee34cd8320ad
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-08-03 23:18:24 +0530

    Add files via upload

---
+++ Programs.h/String_Characters_Frequencies.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    char a[100];
+    int i,b[200]={0},max;
+
+    printf("\nEnter a string : ");
+     gets(a);
+
+     max=a[0];
+     for(i=0;a[i]!='\0';i++)
+        if(max<a[i])
+              max=a[i];
+
+    max++;
+
+    for(i=0;a[i]!='\0';i++)
+       b[a[i]]++;
+
+    for(i=0;i<max;i++)
+      {
+          if(b[i]>0)
+              printf("\nFrequency of '%c' = %d",i,b[i]);
+      }
+
+    return 0;
+
+    
+}
