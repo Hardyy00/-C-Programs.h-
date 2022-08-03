32240e804ecdaf5f97379a91ead3530443a6e7eb
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-08-03 23:17:27 +0530

    Add files via upload

--- Programs.h/String_Character_All_Occurence_Deletion.c
+++ Programs.h/String_Character_All_Occurence_Deletion.c
@@ -2,32 +2,32 @@
 int main()
 {
     char a[100],ch;
-    int i,j,flag=0,p[100],d=0;
+    int i,j,p[100],flag=0,d=0;
 
-    printf("\nEnter a string : ");
-      gets(a);
+    printf("\nEnter the string : ");
+     gets(a);
 
-    printf("\nEnter the character that you want to delete : ");
+    printf("\nEnter the character that you want to delete from the string : ");
       scanf("%c",&ch);
 
-    for(i=0;a[i]!=0;i++)
+    for(i=0;a[i]!='\0';i++)
         if(a[i]==ch)
-           {
-               flag=1;
-               p[d]=i;
-               d++;
-           }
+         {
+             flag=1;
+             p[d]=i;
+             d++;
+         }
 
     if(flag==0)
-        printf("\nThe character doesn't exist in the string.");
-    else
+           printf("\nThe character doesn't exist in the string");
+    else 
        {
            for(i=0;i<d;i++)
-            for(j=p[i]-i;a[j]!=0;j++)
-              a[j]=a[j+1];
+             for(j=p[i]-i;a[j]!='\0';j++)
+                a[j]=a[j+1];
 
-            printf("\nString after deletion : %s",a);
+            printf("\nAfter deletion : %s",a);
        }
 
-         return 0;
+        return 0;
 }
